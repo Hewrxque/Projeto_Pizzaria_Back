95e73b3f432ca0aa25bb38af3e0ee05788476cb4
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-22 21:18:31 -0300

    up

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from "express";
 const router = Router(); 
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ok: true})
+    return res.json({name: 'Henrique'})
+    // throw new Error('Erro ao fazer essa requisiçao')
 })
 
 export { router };
