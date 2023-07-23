2d56b8171b6b4b5ba39d28fbe1cd8133388f22f9
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-23 17:47:05 -0300

    update

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 const router = Router();
 
 //-- Rotas User --
@@ -11,6 +13,6 @@ router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
-router.get("/me", new DetailUserController().handle)
+router.get("/me", isAuthenticated, new DetailUserController().handle)
 
 export { router };
