20f2f353b2ea6c38ba567beb9db3c8c182762bad
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-30 11:09:50 -0300

    up

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,9 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -15,13 +18,15 @@ router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
-router.get("/me", isAuthenticated, new DetailUserController().handle)
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 //-- Rotas Category --
-router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+router.post("/category", isAuthenticated,new CreateCategoryController().handle);
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 //-- Rotas Product --
+router.post("/product", isAuthenticated, new CreateProductController().handle);
+
 
 export { router };
--- src/services/product/CreateProductService.ts
+++ src/services/product/CreateProductService.ts
@@ -1,8 +1,17 @@
 import prismaClient from "../../prisma";
 
-class CreateProductService{
-    async execute(){
+interface ProductRequest{
+    name: string;
+    price: string;
+    description: string;
+    banner: string;
+    category_id: string;
+}
 
+class CreateProductService{
+    async execute({name, price, description, banner, category_id}: ProductRequest){
+        
+        return {ok: true}
     }
 }
 
