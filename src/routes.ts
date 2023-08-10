2972111293d135af98c286ff5e77648acf06b6ba
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-08-10 20:21:48 -0300

    add function detailsOrder

--- src/routes.ts
+++ src/routes.ts
@@ -17,7 +17,9 @@ import { RemoveOrderController } from "./controllers/order/RemoveOrderController
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
+
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
+import { DetailOrderController } from "./controllers/order/DetailOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -69,5 +71,8 @@ router.delete(
 );
 
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
+
 router.get("/orders", isAuthenticated, new ListOrdersController().handle);
+router.get("/order/detail", isAuthenticated, new DetailOrderController().handle);
+
 export { router };
